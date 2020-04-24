95cb843a30096e65bf7e00ef2af3fd2a69fe9cea
Eivind Stray <eivindhstray@gmail.com>
2020-04-24 16:53:01 +0200

    trainer made

--- vowels/vowels.py
+++ vowels/vowels.py
@@ -1,5 +1,6 @@
 import numpy as np
 import extract_classes as ext
+from scipy.stats import multivariate_normal
 
 
 def sample_mean(dataset):
@@ -16,28 +17,41 @@ def sample_mean(dataset):
 
 def cov_matrix(dataset): #CROSS CHECK THAT THIS IS CORRECT!!
     N = len(dataset[0])
+    sample_size = len(dataset)
     mean = sample_mean(dataset)
    
     mean = mean.reshape(1,N)
     cov_matrix = np.zeros((N,N))
     for sample in range (N):
         x=np.asfarray(dataset[sample],float)
-        cov_matrix += (x-mean).T@((x)-mean)
-    return cov_matrix/N
+        cov_matrix += (x-mean).T@((x)-mean)/sample_size
+    return cov_matrix
 
 
-def generate_mean_cov_map(filename):
+def generate_mean_cov_map(filename,start,end):
     mean_cov_map = {}
     classes_map = ext.extract_classes_map(filename)
     for sound in classes_map:
-        mean_cov_map[sound] =[sample_mean(classes_map[sound])]
+        mean_cov_map[sound] =[sample_mean(classes_map[sound[start:end]])]
         
-        mean_cov_map[sound].append(cov_matrix(classes_map[sound]))
+        mean_cov_map[sound].append(cov_matrix(classes_map[sound[start:end]]))
         
 
     return mean_cov_map
 
-print(generate_mean_cov_map("data.dat"))
+print(generate_mean_cov_map("data.dat",0,10))
 
 
+def train_single_gaussian(start,end,x_size,diag = False):
+    probabilities = np.zeros((12, x_size))
+    mean_cov_map = generate_mean_cov_map("data.dat",start,end)
+    for i,sound in enumerate(mean_cov_map):
+        if diag == True:
+            sound[2] = np.diag(sound[2])
+        rv = multivariate_normal(sound[0],sound[2])
+        probabilities[i] = rv
 
+    return probabilities[i]
+
+def predict():
+    
