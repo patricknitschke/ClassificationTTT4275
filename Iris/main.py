8c0013784356d3a4b6b1ea9019019d33ea4decf0
patricknitschke <patrick.nitschke8@gmail.com>
2020-04-27 21:23:53 +0200

    make pretty

--- Iris/main.py
+++ Iris/main.py
@@ -10,29 +10,15 @@ target = iris_data.target
 data = iris_data.data
 
 def task_1a():
-    training_data1 = data[0:30]
-    training_data2 = data[50:80]
-    training_data3 = data[100:130]
-    training_data = [training_data1,training_data2,training_data3]
-    training_target1 = target[0:30]
-    training_target2 = target[50:80]
-    training_target3 = target[100:130]
-    training_target = [training_target1, training_target2, training_target3]
 
-    testing_data1 = data[30:50]
-    testing_data2 = data[80:100]
-    testing_data3 = data[130:150]
-    testing_data = [testing_data1,testing_data2,testing_data3]
-    testing_target1 = target[30:50]
-    testing_target2 = target[80:100]
-    testing_target3 = target[130:150]
-    testing_target = [testing_target1, testing_target2, testing_target3]
+    training_data, training_target, testing_data, testing_target = iris.allocate_data(data, target)
 
     #------------------Training-------------------
     training_ratio, confusion_training, W = iris.assignment_1_trainingset(training_data,training_target,3)
     print("Training:")
     print(training_ratio)
     print(confusion_training)
+    print("W:", W)
 
     #-------------------Testing---------------------
     test_ratio, confusion_test = iris.assignment_1_testingset(W, training_data, testing_data, testing_target, 3)
@@ -43,9 +29,9 @@ def task_1a():
 
 def task_1b():
     training_data1 = data[20:50]
-    training_data2 = data[70:100]
+    training_set_class2 = data[70:100]
     training_data3 = data[120:150]
-    training_data = [training_data1,training_data2,training_data3]
+    training_data = [training_data1,training_set_class2,training_data3]
     training_target1 = target[20:50]
     training_target2 = target[70:100]
     training_target3 = target[120:150]
@@ -119,19 +105,17 @@ def task_2(data, target):
 
 
 
-
-
-
 def main():
-    
     print("Task 1a")
     task_1a()
-    print("\n ----------------------------------- \n")
-    print("Task 1b")
-    task_1b()
+    # print("\n ----------------------------------- \n")
+    # print("Task 1b")
+    # task_1b()
     
-    print("\n ------------------------------------------ \n")
-    print("Task 2")
-    task_2(data, target)
-    
-main()
+    # print("\n ------------------------------------------ \n")
+    # print("Task 2")
+    # task_2(data, target)
+
+
+if __name__ == '__main__':
+    main()
