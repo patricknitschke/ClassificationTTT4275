c06ed09c13a4748cbe1c039b574cd191e2b9bba3
patricknitschke <patrick.nitschke8@gmail.com>
2020-04-27 21:56:37 +0200

    start vowels

---
+++ vowels/vowels2.py
@@ -0,0 +1,10 @@
+''' Classification of pronouced vowels
+ - classify a full set of 12 vowel classes
+ - 139 recordings each class
+ - 70 training, 69 testing
+ 
+ 
+'''
+
+if __name__ == '__main__':
+    print("Starting")
