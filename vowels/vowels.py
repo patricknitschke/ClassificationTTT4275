1124269881f5185aa3c4b9b252fa26615e62ff82
Eivind Stray <eivindhstray@gmail.com>
2020-04-27 11:40:18 +0200

    fixed error

--- vowels/vowels.py
+++ vowels/vowels.py
@@ -36,8 +36,8 @@ def generate_mean_cov_map(filename,start,end):
     classes_map = ext.extract_classes_map(filename)
     list_of_sounds = make_sequence(classes_map)
     for sound in classes_map:
-        mean_cov_map[sound] =[sample_mean(classes_map[sound[start:end]])]
-        mean_cov_map[sound].append(cov_matrix(classes_map[sound[start:end]]))
+        mean_cov_map[sound] =[sample_mean(classes_map[sound][start:end])]
+        mean_cov_map[sound].append(cov_matrix(classes_map[sound][start:end]))
     return mean_cov_map,list_of_sounds
 
 
@@ -56,24 +56,37 @@ def single_gaussian(start,end,diag = False):
     
     mean_cov_map,sound_list = generate_mean_cov_map("data.dat",start,end)
     train_map,test_map = generate_x("data.dat",0,70)
-    probability_vector = np.zeros((12,1))
-    
-    for iterate_class in test_map:
-        for sample in range(30):
+    probability_vector = np.empty((12,1))
+    correct =  0
+    wrong = 0
+    total = 0
+    for iterate_class in train_map:
+        for sample in range(len(train_map[iterate_class])):
             for i,sound in enumerate(mean_cov_map):
                 cov_matrix = mean_cov_map[sound][1]
                 sample_mean = mean_cov_map[sound][0]
+               
                 if diag == True:
                     cov_matrix = np.diag(np.diag(mean_cov_map[sound][1]))
-                probability = multivariate_normal.pdf(test_map[iterate_class][sample],mean = sample_mean,cov = cov_matrix)
+                    probability = multivariate_normal.pdf(train_map[iterate_class][sample],mean = sample_mean,cov = cov_matrix)
+                else:
+                    probability = multivariate_normal.pdf(train_map[iterate_class][sample],mean = sample_mean,cov = cov_matrix,allow_singular = True)
                 probability_vector[i] = probability
-                print(np.argmax(probability_vector))
-                print (sound_list[np.argmax(probability_vector)],"sound:",iterate_class)
+                predicted_index = np.argmax(probability_vector)
+                predicted_sound = sound_list[predicted_index]
+                true_guess = iterate_class
+                print("true:", true_guess, "guessed:",predicted_sound)
+                total += 1
+                if true_guess == predicted_sound:
+                    correct += 1
+                else:
+                    wrong += 1
+        print(correct/total)
 
 
 
 
 
 
+print(single_gaussian(0,70,True))
 
-print(single_gaussian(0,30,True))
